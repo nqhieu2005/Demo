82e83c7ed1165274d93573fc8c334937b8fdd07a
unknown <hieunguyen2005q@gmail.com>
2024-04-04 22:30:18 +0700

    hieu test login lan 1

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const Login = () => {
+
+    const[username, usernameupdate] = useState('');
+    const[password, passwordupdate] = useState('');
+
+    const proceedLogin = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div className="row">
+            
+        </div>
+    )
+}
